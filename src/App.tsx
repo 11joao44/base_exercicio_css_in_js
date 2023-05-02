cf4b79808b25686548bcbc03a16654bfbe5fd9d4
João Pedro <11joao44@gmail.com>
2023-05-02 02:58:13 -0400

    refatorando codigo para styled-components

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components'
+
+import Header from './components/Cabecalho'
+import Hero from './components/Hero'
+import ListaVagas from './containers/ListaVagas'
+import EstiloGlobal, { Container } from './styles'
+import themeLight from './theme/light'
+
+function App() {
+  return (
+    <ThemeProvider theme={themeLight}>
+      <EstiloGlobal />
+      <Header />
+      <Hero />
+      <Container>
+        <ListaVagas />
+      </Container>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,10 @@
+import { Cabecalho as Header } from './styles'
+
+const Cabecalho = () => (
+  <Header>
+    <h1>EBAC Jobs</h1>
+    <button>Trocar Tema</button>
+  </Header>
+)
+
+export default Cabecalho
